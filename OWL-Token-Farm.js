27083ba48bc533d8255c2ddfde98a6b00933f2fa
kaiomm <kaiomm@gmail.com>
2020-07-05 02:21:13 -0300

    Update OWL-Token-Farm.js

--- OWL-Token-Farm.js
+++ OWL-Token-Farm.js
@@ -12,11 +12,19 @@
 (function() {
     window.addEventListener('load', function () {
         var iframe = document.getElementById("home-player-id");
-        var str = iframe.src;
-        str = str.replace("autoplay=0","autoplay=1");
-        iframe.src = str;
-        setTimeout(function(){
-            window.location.reload(1);
-        }, 3600000);
+        if(iframe){
+            var str = iframe.src;
+            str = str.replace("autoplay=0","autoplay=1");
+            iframe.src = str;
+            setTimeout(function(){
+                window.location.reload(1);
+            }, 3600000);
+        } else {
+            var time = document.getElementById("__next").children[0].children[0].children[2].children[0].children[1].children[0].children[0].innerText
+            var millis = Number(time.split(':')[0])*3600000 + Number(time.split(':')[1])*60000 + Number(time.split(':')[2])*1000
+            setTimeout(function(){
+                window.location.reload(1)
+            }, millis)
+        }
     });
 })();
